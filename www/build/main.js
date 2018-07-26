d9e242394bda1d324515e3b3ece7ca60679fd041
Yuda Winata <yudaw@amazon.com>
2018-07-25 23:50:53 -0700

    Add search text

--- www/build/main.js
+++ www/build/main.js
@@ -58,7 +58,7 @@ var HomePage = /** @class */ (function () {
     }
     HomePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-home',template:/*ion-inline-start:"/Users/yudaw/Documents/Project/homecook/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar>\n    <button ion-button menuToggle>\n      <ion-icon name="menu"></ion-icon>\n    </button>\n    <ion-title>Home</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <h3>Ionic Menu Starter</h3>\n\n  <p>\n    If you get lost, the <a href="http://ionicframework.com/docs/v2">docs</a> will show you the way.\n  </p>\n\n  <button ion-button secondary menuToggle>Toggle Menu</button>\n</ion-content>\n'/*ion-inline-end:"/Users/yudaw/Documents/Project/homecook/src/pages/home/home.html"*/
+            selector: 'page-home',template:/*ion-inline-start:"/Users/yudaw/Documents/Project/homecook/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar>\n    <button ion-button menuToggle>\n      <ion-icon name="menu"></ion-icon>\n    </button>\n    <ion-title>homecook</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <ion-input id="myInput" placeholder="What should I eat..."></ion-input>\n  <ion-icon class="icon"ios="ios-search" md="md-search" item-right></ion-icon>\n\n</ion-content>\n'/*ion-inline-end:"/Users/yudaw/Documents/Project/homecook/src/pages/home/home.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]])
     ], HomePage);
