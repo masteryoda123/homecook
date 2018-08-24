7c30e2342993fcc9f83f1773251b9bab88bc5f16
Cheryl Chandra <chandra.cheryl@gmail.com>
2018-08-23 23:48:19 -0700

    add shopping list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { InventoryPage } from '../pages/inventory/inventory';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
 
 @Component({
   templateUrl: 'app.html'
@@ -24,7 +25,8 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'Inventory', component: InventoryPage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Shopping List', component: ShoppingListPage }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { DetailPage } from '../pages/detail/detail';
 import { InventoryPage } from '../pages/inventory/inventory';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     DetailPage,
-    InventoryPage
+    InventoryPage,
+    ShoppingListPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     DetailPage,
-    InventoryPage
+    InventoryPage,
+    ShoppingListPage
   ],
   providers: [
     StatusBar,
